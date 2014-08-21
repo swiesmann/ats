61092a14c9b877e36f21ace13ea7ee2c5a2e4140
klappstuhl <supagubby@gmx.de>
2014-08-21 17:03:00 +0200

    Hello World

---
+++ src/ats/serv.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask.templating import render_template
+
+app = Flask(__name__)
+config = {
+    "DEBUG" : True
+}
+app.config.update(config)
+
+@app.route("/")
+def hello_word():
+    output = render_template(
+        "index.html"
+    )
+    return output
+app.run()
